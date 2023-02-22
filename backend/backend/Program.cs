40c1e557f1d9fcb1f62d55a68d10110b348785a6
Maksim Khirug <maxkhirug@gmail.com>
2023-02-23 01:50:42 +0300

    Подлкючена БД, настроен сериалайзер для таблицы Students

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,9 +1,9 @@
+using Newtonsoft.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -19,12 +19,17 @@ builder.Services.AddCors(options => {
     {
         builder.WithOrigins(frontendUrl).AllowAnyMethod().AllowAnyHeader();
     });
+
 });
+
+builder.Services.AddControllersWithViews().AddNewtonsoftJson(options => 
+    options.SerializerSettings.ReferenceLoopHandling=Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+        .AddNewtonsoftJson(options =>options.SerializerSettings.ContractResolver 
+        = new DefaultContractResolver());
 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
